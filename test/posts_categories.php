b8c067521e0b959865cb6d97669107b7ef75bf64
alexvidal <contato@webav.com.br>
2022-08-15 10:02:01 -0300

    create test

---
+++ test/posts_categories.php
@@ -0,0 +1,14 @@
+<?php
+use Erikai\Migration;
+require "vendor/autoload.php";
+$create = new Migration();
+$create->table('posts_categories');
+$create->column('id')->type('int(11)')->default();
+$create->column('id_user')->type('int(11)')->default();
+$create->column('title')->type('varchar(255)')->default();
+$create->column('created_at')->type('timestamp')->default("current_timestamp()");
+$create->column('updated_at')->type('timestamp')->default("current_timestamp() ON UPDATE current_timestamp()");
+$create->save();
+$create->primary('id');
+$create->autoIncrement('id');
+$create->addKey('users_categories', "id_user", "users", "id");
